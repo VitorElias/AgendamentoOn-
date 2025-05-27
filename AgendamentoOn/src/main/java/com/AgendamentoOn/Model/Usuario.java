a5881423300ad2fd48f59ee0346561483984623b
Gabriel Nicolau de Sousa <gabrielnicolllau1@gmail.com>
2025-05-26 21:39:47 -0300

    fix: removendo atributos não usados na classe Usuario e Repository do Usuario

--- AgendamentoOn/src/main/java/com/AgendamentoOn/Model/Usuario.java
+++ AgendamentoOn/src/main/java/com/AgendamentoOn/Model/Usuario.java
@@ -39,34 +39,26 @@ public class Usuario {
     @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$", message = "Senha deve conter ao menos 1 maiúscula, 1 número e 1 caractere especial")
     private String senha;
-    private int idade;
-
-    private String cpf;
 
     private String telefone;
-
-    private String endereco;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Agendamento> agendamentos =  new ArrayList<>();
+    private List<Agendamento> agendamentos = new ArrayList<>();
 
     public Usuario() {
-        this.role = Role.ADMINISTRADOR;  
+        this.role = Role.ADMINISTRADOR;
     }
 
     public Usuario(@NotNull @Size(min = 3, max = 100) String nome, @NotNull String email, @NotNull String senha,
-                   @NotNull int idade, @NotNull String cpf, @NotNull String telefone, @NotNull String endereco, Role role) {
+            @NotNull String telefone, Role role) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.idade = idade;
-        this.cpf = cpf;
         this.telefone = telefone;
-        this.endereco = endereco;
-        this.role = role != null ? role : Role.ADMINISTRADOR; 
+        this.role = role != null ? role : Role.ADMINISTRADOR;
     }
 
     public Long getId() {
@@ -101,36 +93,12 @@ public class Usuario {
         this.senha = senha;
     }
 
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getTelefone() {
         return telefone;
     }
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
     }
 
     public List<Agendamento> getAgendamentos() {
